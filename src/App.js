e78169e945f08336ac95dd3d4f32435e938fbe44
John Robert Thurlby <johnrthurlby@gmail.com>
2018-06-22 15:32:42 -0400

    First update of code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
     <Section />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>Ⓒ John R. Thurlby</p>
+	</header>
+);
+
+export default Footer;
