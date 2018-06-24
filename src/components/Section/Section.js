4acb4d6efb58b737d5cc33807a90a84a320eea76
John Robert Thurlby <johnrthurlby@gmail.com>
2018-06-24 16:33:30 -0400

    Added click functionality

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,16 +4,19 @@ import "./Section.css";
 // By importing the Section.css file, it is added to the DOM whenever this component loads
 
 const Section = props => (
-	<div 
-	className="allCards" 
-	key={props.id}
-	onClick={() => props.handleClick(props.id, props.clicked)}
-	>
-		<img 
-		id={props.id}
-		src={props.image}
-		alt={props.name}
-		/>
+	<div class="row">
+		<div class="column images"
+			key={props.id}
+			onClick={() => props.handleClick(props.id, props.clicked)}
+			>
+			<img 
+				id={props.id}
+				src={props.image}
+				alt={props.name}
+				width="250px"
+				height="250px"
+			/>
+		</div>
 	</div>
 );
 
